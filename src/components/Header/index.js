be9be602709684dd37fa80ff1477f42bbe50ab8a
NickNocc <haretakis4@gmail.com>
2022-03-15 15:09:42 -0700

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Nav from '../Nav';
 
 function Header() {
+    const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
     return (
         <header className='flex-row px-1'>
             <h1>Nick Haretakis</h1> <Nav></Nav>
