cca473ed7729a5affd5cfd27ea7930b303ee9de2
NickNocc <haretakis4@gmail.com>
2022-03-27 17:37:35 -0700

    Bug with deployment

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.94932da7e1114fe84695208f6c8939b2.js"
+  "/Nick-Haretakis-portfolio/precache-manifest.d77899ffc0c28adc22b86d4a2556ea76.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Nick-Haretakis-portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
